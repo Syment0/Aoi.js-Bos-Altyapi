f842e98c445ccbe5728964f2bf68d9af9a7ebdc4
Syment <83902370+Sment02@users.noreply.github.com>
2021-09-02 15:33:45 +0300

    Add files via upload

---
+++ NumZ.js
@@ -0,0 +1,14 @@
+const aoijs = require('aoi.js');
+const bot = new aoijs.Bot({
+	token: "", //Token
+	prefix: [''] // Prefix
+});
+
+bot.onMessage();
+bot.loadCommands(`./komutlar/`);
+
+bot.command({
+    name:"deneme",
+    code:`UwU İyi Kodlamalar`
+    })
+    
